e26beafff9d4115062855a398746d20d38d84843
Aleksi Lampela <aleksi.lampela@live.fi>
2021-04-06 18:29:26 +0300

    Added new files

--- demo.js
+++
@@ -1,7 +0,0 @@
-alert("Hello World!");
-console.log("Hello World!");
-var cars = ["Audi", "BMW", "VW"];
-
-for (var i = 0; i < cars.length; i++) {
-    console.log(cars[i]);
-}
---
+++ scripts.js
@@ -0,0 +1,67 @@
+// Lisää tapahtumakuuntelijat
+document.querySelector(".inputField button").addEventListener("click", newLine);
+document.querySelector(".footer button").addEventListener("click", clear);
+
+// Luo sulkemisnapin ja lisää sen listan kohteisiin
+var todoList = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < todoList.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("X");
+  span.className = "deleteButton";
+  span.appendChild(txt);
+  todoList[i].appendChild(span);
+}
+
+// Piilota kohde listasta
+var hide = document.getElementsByClassName("deleteButton");
+var i;
+for (i = 0; i < hide.length; i++) {
+    hide[i].onclick = function() {
+        var div = this.parentNode;
+        div.style.display = "none";
+    }
+}
+
+// Lisää valintamerkki klikattaessa listan kohdetta
+    var list = document.querySelector('ul');
+    list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+        }
+    }, false);
+
+// Lisää uusi kohde listaan
+function newLine() {
+    var li = document.createElement("li");
+    var inputValue = document.querySelector(".inputField input").value;
+    var text = document.createTextNode(inputValue);
+    li.appendChild(text);
+    if (!/\S/.test(inputValue)) {
+        alert("Et voi lisätä tyhjää kenttää");
+    } else {
+        document.querySelector(".todoList").appendChild(li);
+    }
+    document.querySelector(".inputField input").value = "";
+    
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("X");
+    span.className = "deleteButton";
+    span.appendChild(txt);
+    todoList[i].appendChild(span);
+
+    for (i = 0; i < hide.length; i++) {
+        hide[i].onclick = function() {
+          var div = this.parentNode;
+          div.style.display = "none";
+        }
+    }
+}
+
+// Tyhjennä koko lista
+function clear() {
+    var nodes = document.querySelector("ul");
+    while (nodes.firstChild) {
+        nodes.removeChild(nodes.firstChild);
+  }
+}
